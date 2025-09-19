f549663233d0c5da29a2bf45ec9b460119634f5f
roobeen-b <rubinbaidhya@gmail.com>
2025-09-19 23:50:01 +0545

    Changed wildcard route for production env to use regex workaround for express v5 issue of not being able to use wildcard directly

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
+  app.get(/^\/(?!api\/).*/, (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }

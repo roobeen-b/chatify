b875876e3c399b94c6e6d7904a371523ff65325c
roobeen-b <rubinbaidhya@gmail.com>
2025-09-22 19:09:40 +0545

    Added missed NODE_ENV key in env.js file

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -8,4 +8,5 @@ export const ENV = {
   RESEND_API_KEY: process.env.RESEND_API_KEY,
   EMAIL_FROM: process.env.EMAIL_FROM,
   EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+  NODE_ENV: process.env.NODE_ENV,
 };
